cebf75053919283ab95cd55f5150fba5bfbb5b3a
Esteban Alazate <esteban.alzate@babel.es>
2017-04-17 14:12:04 +0200

    this cambio

--- this.js
+++ this.js
@@ -11,5 +11,12 @@ const coche = new Coche();
 coche.getRueda();
 
 // no accede a la funcion porque no esta en la funcion
-const  numRueda = coche.getRueda;
+//const  numRueda = coche.getRueda; //coche.getRuedas.bind(coche); de esta manera le pones un this fijo y que no cambie
+const  numRueda = coche.getRueda.bind(coche);
 numRueda(); 
+
+//setTimeout(coche.getRueda,2000); sale undefined porque la funcion la llama timeOUT y no tiene capacidad para entrar
+
+
+//const camion = { rueda:8, getRueda: coche.getRueda} 
+//camion.getRueda();
